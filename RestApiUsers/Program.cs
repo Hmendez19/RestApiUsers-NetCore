342303bd7881996755c6c4f418baded9b79b94e0
Hmendez19 <herwinmendezm@gmail.com>
2023-12-13 20:15:01 -0600

    feat (Program.cs): adding information to the documentation with swagger.

--- RestApiUsers/Program.cs
+++ RestApiUsers/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(doc =>
+{
+    doc.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Gestión de Usuarios API",
+        Version = "v1",
+        Description="API que permite la gestión de operaciones relacionadas con usuarios.Proporciona endpoints para crear y obtener información de usuarios. La documentación de la API incluye información detallada sobre estos endpoints, como sus parámetros de entrada y respuestas esperadas.",
+        Contact = new OpenApiContact
+        {
+            Name = "Herwin Mendez"
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +26,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json",  "Gestión de usuarios API");
+    });
 }
 
 app.UseHttpsRedirection();
